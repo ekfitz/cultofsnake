5eacce5e06ae39cba8f4322c5d3ee91e5503e9d1
Eric <57112072+ekfitz@users.noreply.github.com>
2019-11-27 22:55:23 -0700

    Mobile
    Javascript mobile redirect

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # cultofsnake
+
+# Nov 27
+
+mobile redirect. Created new files for remaining html main and mobile.
+
 remod entire repository
 
 Custom pro audio website
--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function showMenu() {
 function setMobile() {
   var w = window.innerWidth;
 
-  if (w > 1000) {
-    window.location.replace("index.html");
+  if (w < 1000) {
+    window.location.replace("m.html");
   }
 }
